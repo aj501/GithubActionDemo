ae861984990c526f02fceca253272b316cde233c
Alex Pham <alex.j.pham@gmail.com>
2023-10-11 22:13:42 -0500

    add Test functions

---
+++ test.py
@@ -0,0 +1,14 @@
+# create simple calculator using python
+# multiply, divide, addition, subtraction
+# create function for each operation
+# Multiplication
+def multiply(a,b):
+    return a*b
+multiply(2,3)
+print(multiply(2,3))
+
+# Division
+def divide(a,b):
+    return a/b 
+divide(6,0)
+print(divide(6,0))
